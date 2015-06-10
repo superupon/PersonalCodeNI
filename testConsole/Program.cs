408da18cbb7bed7f1b2889ce82ee1d0df1a3f79d
Yuchen Zhang <superupon@126.com>
2015-06-10 11:54:01 +0800

    <dev>create a new test project to test git</dev>

---
+++ testConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    public class Program
+    {
+        enum WhatEver
+        {
+            HALO = 0,
+            hello = 1,
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(((WhatEver)0).ToString());
+            int j = Program.Add(4, 5);
+        }
+        public static int Add(int a, int b)
+        { 
+            return a + b;
+        }
+        public void HaHa(string str)
+        {
+            Console.WriteLine( "Haha");
+        }
+    }
+}
